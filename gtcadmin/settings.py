902b97f66b49b1d00332e9db014cd025eb818eab
Isaac Rabiu <ziktech2020@gmail.com>
2021-08-08 12:34:03 +0100

    updated settings with whitenoise

--- gtcadmin/settings.py
+++ gtcadmin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,7 +123,7 @@ STATICFILES_DIRS = [
 BASE_DIR / "static"
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR/"asert/")
+STATIC_ROOT = os.path.join(BASE_DIR/"asert/",'staticfiles')
 
 MEDIA_ROOT = (BASE_DIR / 'media')
 
